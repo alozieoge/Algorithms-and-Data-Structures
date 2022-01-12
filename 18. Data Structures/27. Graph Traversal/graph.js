6a9773a10024fe95d3f7e398342d9c6515dd9330
alozieoge <39676713+alozieoge@users.noreply.github.com>
2022-01-12 23:25:42 +0000

    Update graph.js
    Implement iterative depth-first traversal algorithm for the Graph class.

--- 18. Data Structures/27. Graph Traversal/graph.js
+++ 18. Data Structures/27. Graph Traversal/graph.js
@@ -69,23 +69,98 @@ class Graph { // Undirected
         // Return the result array. 
 
         // MEE
-        var result = [];
-        var visited = {};
+//         var result = [];
+//         var visited = {};
+//         const adjacencyList = this.adjacencyList;
+
+//         function DFS(vertex) {
+//             if (!vertex) return;
+//             visited[vertex] = true;
+//             result.push(vertex);
+//             for (var adjacentVertex of adjacencyList[vertex]) {
+//                 if (!visited[adjacentVertex]) {
+//                     DFS(adjacentVertex);
+//                 }
+//             }
+//         }
+
+//         DFS(startVertex);
+//         return result;
+        
+        // CS
+        const result = [];
+        const visited = {};
         const adjacencyList = this.adjacencyList;
 
-        function DFS(vertex) {
-            if (!vertex) return;
+        (function DFS(vertex) {
+            if (!vertex) return null;
             visited[vertex] = true;
             result.push(vertex);
-            for (var adjacentVertex of adjacencyList[vertex]) {
-                if (!visited[adjacentVertex]) {
-                    DFS(adjacentVertex);
+            adjacencyList[vertex].forEach(neighbor => {
+                if (!visited[neighbor]) {
+                    return DFS(neighbor);
                 }
-            }
-        }
+            })
+        })(startVertex);
 
-        DFS(startVertex);
         return result;
+    }
+
+    DFSIterative(startVertex) {
+        // Write a function that accepts a starting node. 
+        // Create a stack to help use keep track of vertices (use a list/array)
+        // Create a list / array to store the end result, to be returned at the very end
+        // Create an object to store the visited vertices. 
+        // Add the starting vertex to the stack, and mark it as visited. 
+        // While the stack has something in it:
+        // - Pop the next vertex from the stack. 
+        // - If that vertex hasn't been visted yet:
+        //   - Mark it as visited. 
+        //   - Add it to the result list. 
+        //   - Push all of its neighbors into the stack. 
+
+        // MEE
+//         var stack = [];
+//         var result = [];
+//         var visited = {};
+//         var vertex = "";
+
+//         stack.push(startVertex);
+//         while (stack.length > 0) {
+//             console.log(stack);
+//             vertex = stack.pop();
+//             if (!visited[vertex]) {
+//                 visited[vertex] = true;
+//                 result.push(vertex);
+//                 this.adjacencyList[vertex].forEach(neighbor => {
+//                     stack.push(neighbor);
+//                 });
+//             }
+//         }
+//         return result;
+
+        // CS
+        const stack = [startVertex];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+
+        visited[startVertex] = true;
+        while(stack.length) {
+            console.log(stack);
+            currentVertex = stack.pop();
+            result.push(currentVertex);
+
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    stack.push(neighbor);
+                }
+            })
+        }
+        return result;
+    }
+
 
     }
 
